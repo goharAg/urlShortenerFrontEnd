cc3092b9ebf8d820621fff67bc21f5bf1961c8c1
Davtyan360 <76738628+Davtyan360@users.noreply.github.com>
2021-05-04 11:15:08 +0400

    Update app-routing.module.ts

--- frontSide/src/app/app-routing.module.ts
+++ frontSide/src/app/app-routing.module.ts
@@ -1,19 +1,9 @@
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-
-// const routes: Routes = [];
-
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main/main.component'
-//import { SearchuserComponent} from "./searchuser/searchuser.component"
+
 
 const routes: Routes = [
   { path: '', component: MainComponent }
